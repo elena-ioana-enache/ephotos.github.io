3acbc829f6ce44c8aced0317b4fef2117ab2e166
Elena Enache <elena.enache@seamless.se>
2020-10-02 20:17:48 +0300

    Updates

---
+++ precache-manifest.e7c95f9b4433e47f61cac606ed827a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94f25f409977b28a32d0069688fbb31",
+    "url": "/ephotos/index.html"
+  },
+  {
+    "revision": "cf8e176e26651f054145",
+    "url": "/ephotos/static/css/main.15f7b3fb.chunk.css"
+  },
+  {
+    "revision": "79c6487f42e686f47479",
+    "url": "/ephotos/static/js/2.0534110f.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/ephotos/static/js/2.0534110f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8e176e26651f054145",
+    "url": "/ephotos/static/js/main.8e416b22.chunk.js"
+  },
+  {
+    "revision": "6372c7a20e21581004cb",
+    "url": "/ephotos/static/js/runtime-main.0885e872.js"
+  }
+]);
